29d1464d9d39179f80cf3e256cb4d3b4ea96cecc
os5c <os.group5c@gmail.com>
2013-04-26 14:00:57 +0530

    Update createContainers.sh

--- scripts11_4/mainServer101/createContainers.sh
+++ scripts11_4/mainServer101/createContainers.sh
@@ -5,6 +5,6 @@ else
 echo "Creating container"+$1;
 swift post $1;
 swift post -r '.r:*' $1
-sudo sshpass -p ubuntu ssh -o StrictHostKeyChecking=no stack@192.168.100.107 'bash -s' < createCacheContainer.sh $1
-sudo sshpass -p ubuntu ssh -o StrictHostKeyChecking=no stack@192.168.100.105 'bash -s' < createCacheContainer.sh $1
+sudo sshpass -p ubuntu ssh -o StrictHostKeyChecking=no stack@$2 'bash -s' < createCacheContainer.sh $1
+sudo sshpass -p ubuntu ssh -o StrictHostKeyChecking=no stack@$3 'bash -s' < createCacheContainer.sh $1
 fi
